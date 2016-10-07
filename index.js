6ce72b1278cc5d7fb227099fa794762844652318
mitch3x3 <mitch3x3@github.com>
2016-10-07 13:15:36 -0700

    Added logo and skeleton for linked pages
    Changes:
    - Added fancy logo
    - Added About page
    - Added skeleton to all other linked pages

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', function(request, response) {
     //response.send('')
     response.sendFile(__dirname + '/index.html');
 })
+app.get('/about', function(request, response) {
+    //response.send('')
+    response.sendFile(__dirname + '/about.html');
+})
+app.get('/submit', function(request, response) {
+    //response.send('')
+    response.sendFile(__dirname + '/submit.html');
+})
 
 app.listen(app.get('port'), function() {
     console.log("Node app is running at localhost:" + app.get('port'))
